cb3db0dea851c3614af8bfe58ad33ab3d9609e9c
mikhail-kalashnikov <hoangk62uet@gmail.com>
2020-07-16 15:35:58 +0700

    change something

--- src/components/Demo/Demo.js
+++ src/components/Demo/Demo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 export default class Demo extends Component {
   //Gọi constructor của parent class Component
   constructor(props) {
@@ -13,6 +14,19 @@ export default class Demo extends Component {
   }
   componentDidMount() {
     console.log('[Demo.js] componentDidMount');
+    this.getUserInfor();
+  }
+  getUserInfor = () => {
+    axios
+      .get('http://jsonplaceholder.typicode.com/users')
+      .then((response) => this.setState({ ...response }));
+    console.log(this.state);
+  };
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.state.list === nextState.list;
+  }
+  componentWillUpdates(nextProps, nextState) {
+    console.log('[Demo.js] ComponentWillUpdate');
   }
   render() {
     console.log('[Demo.js] rendering,,,');
