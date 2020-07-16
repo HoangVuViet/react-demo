eeb418dadda16d9130815f9ea96359bd8ecf4648
mikhail-kalashnikov <hoangk62uet@gmail.com>
2020-07-16 22:55:06 +0700

    set up something

--- src/components/Demo/Demo.js
+++ src/components/Demo/Demo.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 export default class Demo extends Component {
-  //Gọi constructor của parent class Component
+  // Calling the constructor of
+  // Parent Class React.Component
   constructor(props) {
-    // Qúa trình khởi tạo
-    // Bao gồm set up state và props
+    // Initialization process
     super(props);
     this.state = { name: 'Vu', list: [], check: false };
   }
@@ -14,44 +14,44 @@ export default class Demo extends Component {
   }
   componentDidMount() {
     console.log('[Demo.js] componentDidMount');
-    this.getUserInfor();
+    // this.getUserInfor();
   }
-  getUserInfor = () => {
-    axios.get('http://jsonplaceholder.typicode.com/users').then((response) => {
-      this.setState({ list: response });
-    });
-  };
-  //Updating
-  componentWillReceiveProps(nextProps, nextState) {
-    console.log('[Demo.js] componentWillReceiveProps');
-  }
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('[Demo.js] shouldComponentUpdate: ' + nextState.name);
-    return true;
-  }
-  componentWillUpdate(nextProps, nextState) {
-    console.log('[Demo.js] componentWillUpdate: ' + nextState.name);
-  }
-  componentDidUpdate(prevProps, prevState) {
-    console.log('[Demo.js] componentDidUpdate: ' + prevState.name);
-  }
-  toggleNameHandler = () => {
-    let checkName = this.state.check;
-    if (!this.state.check) {
-      this.setState({ name: 'Viet', check: !checkName });
-    } else {
-      this.setState({ name: 'Hoang', check: !checkName });
-    }
-  };
-  //Unmounting
-  componentWillUnmount() {
-    console.log('[Demo.js] componentWillUnmount');
-  }
+  // getUserInfor = () => {
+  //   axios.get('http://jsonplaceholder.typicode.com/users').then((response) => {
+  //     this.setState({ list: response });
+  //   });
+  // };
+  // //Updating
+  // componentWillReceiveProps(nextProps, nextState) {
+  //   console.log('[Demo.js] componentWillReceiveProps');
+  // }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('[Demo.js] shouldComponentUpdate: ' + nextState.name);
+  //   return true;
+  // }
+  // componentWillUpdate(nextProps, nextState) {
+  //   console.log('[Demo.js] componentWillUpdate: ' + nextState.name);
+  // }
+  // componentDidUpdate(prevProps, prevState) {
+  //   console.log('[Demo.js] componentDidUpdate: ' + prevState.name);
+  // }
+  // toggleNameHandler = () => {
+  //   let checkName = this.state.check;
+  //   if (!this.state.check) {
+  //     this.setState({ name: 'Viet', check: !checkName });
+  //   } else {
+  //     this.setState({ name: 'Hoang', check: !checkName });
+  //   }
+  // };
+  // //Unmounting
+  // componentWillUnmount() {
+  //   console.log('[Demo.js] componentWillUnmount');
+  // }
   render() {
     console.log(this.state);
     console.log('[Demo.js] rendering,,,');
     return (
-      <div>
+      <div className="Demo">
         <button onClick={this.toggleNameHandler}>Name Toggle</button>
         <p>{this.state.name}</p>
         <p>{this.props.children}</p>
